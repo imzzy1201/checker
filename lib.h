e0eb8e574cb6145318aa4b2bb44913cc0d2a5f79
imzzy1201 <38782715+imzzy1201@users.noreply.github.com>
2020-05-27 20:57:00 +0800

    v0.1.0

--- lib.h
+++ lib.h
@@ -1,85 +1,157 @@
-#define OS_WIN
-/**************
- * "WIN"	: Windows all
- * "LINUX"	: Linux all
- * "MACOS"	: Mac OS*(uncompleted)
- */
-
-#include<bits.stdc++.h>
+#include<bits/stdc++.h>
+#define Int32 int
+#define Int64 long long
+#define uInt32 unsigned int
+#define uInt64 unsigned long long
 using namespace std;
+string PATH, FNAME;
+void exit_all(int);
 
-string PATH;
 
-#ifdef OS_WIN
 namespace SYS
 {
-	#include<windows.h>
-	
-	const int CRED    =FOREGROUND_INTENSITY|FOREGROUND_RED;
-	const int CGREEN  =FOREGROUND_INTENSITY|FOREGROUND_GREEN;
-	const int CBLUE   =FOREGROUND_INTENSITY|FOREGROUND_BLUE;
-	const int CYELLOW =FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN;
-	const int CWHITE  =FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE;
-	
-	inline void SetFontColor(int c) {SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),c);}
-	inline void title(string t) {system(("title "+t).c_str());}
-	inline void mkdir(string t) {system(("mkdir \""+t+'\"').c_str());}
-	inline void mkfile(string t) {system(("cd.>\""+t+'\"').c_str());}
-	inline int run(string t) {return system(t.c_str());}
-	inline string getpath()
+#define OS_WIN 1
+
+#ifdef OS_WIN
+#include<windows.h>
+#include<conio.h>
+	const int CRED    = FOREGROUND_INTENSITY | FOREGROUND_RED;
+	const int CGREEN  = FOREGROUND_INTENSITY | FOREGROUND_GREEN;
+	const int CBLUE   = FOREGROUND_INTENSITY | FOREGROUND_BLUE;
+	const int CYELLOW = FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN;
+	const int CWHITE  = FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+	void SetColor(int c) {SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), c);}
+	void title(string t) {system(("title " + t).c_str());}
+	void mkdir(string t) {system(("mkdir \"" + t + '\"').c_str());}
+	void mkfile(string t) {system(("cd.>\"" + t + '\"').c_str());}
+	int run(string t) {return system(t.c_str());}
+	void pause() {system("pause");}
+	pair<string, string> getpath()
 	{
-		string res=_pgmptr;
-		for(rgi i=res.length()-1;i>0;--i)
-			if(res[i]!='\\') res.pop_back();
-			else {res.pop_back(); break;}
-		return res;
+		string res1 = _pgmptr, res2 = "";
+		for(int i = 0; i < res1.length(); ++i) if(res1[i] == '\\') res1[i] = '/';
+		for(int i = res1.length() - 1; i > 0; --i)
+			if(res1[i] != '/') res2 = res1[i] + res2, res1.pop_back();
+			else {res1.pop_back(); break;}
+		return make_pair(res1, res2);
 	}
-}
 #endif
+
+#ifdef OS_LINUX
+#include<unistd.h>
+#include<termio.h>
+	const char *CRED    = "\033[1;31m";
+	const char *CGREEN  = "\033[1;32m";
+	const char *CYELLOW = "\033[1;33m";
+	const char *CBLUE   = "\033[1;34m";
+	const char *CWHITE  = "\033[1;37m";
+	void SetColor(const char *c) {printf("%s", c);}
+	void title(string t) {cout << "\033]0;" << t << "\007";}
+	void mkdir(string t) {system(("mkdir \"" + t + '\"').c_str());}
+	void mkfile(string t) {system(("touch\"" + t + '\"').c_str());}
+	int run(string t) {return system(("\"./" + t + "\"").c_str());}
+	int getch()
+	{
+		struct termios tm, tm_old;
+		int fd = 0, ch;
+		if (tcgetattr(fd, &tm) < 0) return -1;
+		tm_old = tm;
+		cfmakeraw(&tm);
+		if (tcsetattr(fd, TCSANOW, &tm) < 0) return -1;
+		ch = getchar();
+		if (tcsetattr(fd, TCSANOW, &tm_old) < 0) return -1;
+		return ch;
+	}
+	void pause() {cout << "Press any key to continue . . .", getch(), cout << endl;}
+	pair<string, string> getpath()
+	{
+		char *tmp = new char[100004];
+		readlink("/proc/self/exe", tmp, 100002);
+		string res1 = tmp, res2 = "";
+		delete[] tmp;
+		for(int i = 0; i < res1.length(); ++i) if(res1[i] == '\\') res1[i] = '/';
+		for(int i = res1.length() - 1; i > 0; --i)
+			if(res1[i] != '/') res2 = res1[i] + res2, res1.pop_back();
+			else {res1.pop_back(); break;}
+		return make_pair(res1, res2);
+	}
+#endif
+} using namespace SYS;
+
+
+namespace RANDOM
+{
+	default_random_engine random_root;
+	void init()
+	{
+		std::mt19937 tmproot(std::chrono::steady_clock::now().time_since_epoch().count());
+		random_root.seed(tmproot() + 1201);
+	}
+	template<typename T> T random(T l, T r)
+	{
+		if(r < l) exit_all(3325);
+		uniform_int_distribution<T> u(l, r);
+		return u(random_root);
+	}
+} using namespace RANDOM;
+
+
+namespace LOGTIME
+{
+#include<sys/time.h>
+	char szTime[30];
+	string logTime()
+	{
+		struct tm *ptm;
+		struct timeb stTimeb;
+		szTime[0] = '\0';
+		ftime(&stTimeb);
+		ptm = localtime(&stTimeb.time);
+		sprintf(szTime, "%04d-%02d-%02d %02d:%02d:%02d.%03d\0",
+				ptm->tm_year + 1900, ptm->tm_mon + 1, ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec, stTimeb.millitm);
+		return string(szTime);
+	}
+} using namespace LOGTIME;
+
+
+namespace LOG
+{
+	ofstream logF;
+	void init()
+	{
+		logF.open(PATH + "/temp/" + FNAME + ".log", ios::app);
+		if(!logF) exit_all(1345);
+		logF << "[o] " << logTime() << ' ' << "STARTED_SECCESSFULLY: " << PATH << ' ' << FNAME << endl;
+	}
+	void stop() {logF.close();}
+} using namespace LOG;
+
 
 void exit_all(int exitVal)
 {
-	if(exitval==0) exit(0);
-	SYS::SetFontColor(CRED);
-	cout<<"checker received an error, ERRCODE: "<<exitVal<<endl;
-	cout<<"Please read ERRCODE.md to solve it"<<endl;
-	cout<<"You can also upload \'temp/ERR.log\' to us"<<endl
+	if(exitVal == 0)
+	{
+		logF << "[o] " << logTime() << ' ' << "EXITED_NORMALLY: " << exitVal << endl << endl;
+		LOG::stop();
+		SetColor(CWHITE);
+		exit(0);
+	}
+	SetColor(CRED);
+	cout << "Program received an error, ERRCODE: " << exitVal << endl;
+	cout << "Please read ERRCODE.md to find out what's wrong. ";
+	cout << "You can also upload \'temp/*.log\' to us at GitHub to find more information about this." << endl;
+	if(logF) logF << "[x] " << logTime() << ' ' << "ABORTED: " << exitVal << endl << endl;
+	LOG::stop();
+	SetColor(CWHITE);
+	pause();
+	exit(exitVal);
 }
 
-namespace random
-{
-#define int32 int
-#define int64 long long
-#define uint32 unsigned int
-#define uint64 unsigned long long
-	std::mt19937 root1(std::chrono::steady_clock::now().time_since_epoch().count());
-	std::mt19937_64 root2(std::chrono::steady_clock::now().time_since_epoch().count());
-	uint32 ruint32(uint32 l, uint32 r) {
-		assert(r >= l);
-		return root1() % (r - l + 1) + l;
-	}
-	uint64 ruint64(uint32 l, uint32 r) {
-		assert(r >= l);
-		return root2() % (r - l + 1) + l;
-	}
-	int32 rint32(int l, int r) {
-		assert(r >= l);
-		return (int32)ruint32(l, r);
-	}
-	int64 rint64(int64 l, int64 r) {
-		assert(r >= l);
-		return (int64)ruint64(l, r);
-	}
-#undef int32
-#undef int64
-#undef uint32
-#undef uint64
-}
 
-using namespace random;
-
-inline void INIT()
+void INIT()
 {
-	PATH=;
-	
+	pair<string, string> p = getpath();
+	PATH = p.first, FNAME = p.second;
+	RANDOM::init();
+	LOG::init();
 }
--- tmp.cpp
+++ tmp.cpp
@@ -1,7 +1,23 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+#include"lib.h"
+// #include<sys/time.h>
 using namespace std;
+
 signed main()
 {
-	cout<<_pgmptr<<endl;
+	INIT();
+	cout<<PATH<<' '<<FNAME<<endl;
+	cout<<random(-100,-1)<<endl;
+	SetColor(CYELLOW);
+	title("aaa");
+	logF<<"hey"<<endl;
+	cout<<logTime()<<endl;
+	Sleep(100);
+	cout<<logTime()<<endl;
+	pause();
+	SetColor(CBLUE);
+	cout<<random(100000000000ll,1000000000000ll)<<endl;
+	SetColor(CWHITE);
+	exit_all(0);
 	return 0;
 }
